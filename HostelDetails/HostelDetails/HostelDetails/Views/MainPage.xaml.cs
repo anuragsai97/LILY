a867b7d64f005e5088444e40ea7e403dbb84289b
Sai Anurag <anuragsai97@gmail.com>
2017-10-20 08:56:37 +0530

    Hostel Details

---
+++ HostelDetails/HostelDetails/HostelDetails/Views/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Xamarin.Forms;
+
+namespace HostelDetails.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        string name="Anurag";
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            //string hostel_details;
+            Uri uri = new Uri("http://192.168.1.37/hostel.php?name="+name);
+            WebRequest request = WebRequest.Create(uri);
+            request.BeginGetResponse((result) =>
+            {
+                try
+                {
+                    Stream stream = request.EndGetResponse(result).GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        
+                        hello.Text = reader.ReadToEnd();
+                        //hostel_details = reader.ReadToEnd();
+
+                    });
+                }
+                catch (Exception exc)
+                {
+                }
+            }, null);
+
+
+
+
+            
+        }
+    }
+}
